74b5119b484820247b5a25db8b4a3b350ba508e8
nitishdhawan <37472048+nitishdhawan@users.noreply.github.com>
2018-04-14 23:43:34 +0530

    Add files via upload

---
+++ assignment2.4.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[15]:
+
+
+word="""WE, THE PEOPLE OF INDIA
+              having solemnly resolve to constitute india into a SOVEREIGN,!
+                    SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC
+                     and to secure all its citizens:
+"""
+print(word)
+    
+    
+
